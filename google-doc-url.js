bff44c57cb952461f5965174591a2cf6de0b1ace
kbarr103 <122462391+kbarr103@users.noreply.github.com>
2023-02-10 09:36:20 -0500

    Update google-doc-url.js
    Changed the link to my actual google sheets

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/14JbN92Bcucb6Z7eUOatMH05xNxqKhwHbP1g8SO8988Q/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
